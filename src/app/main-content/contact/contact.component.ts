784eb61a7c1465a009d620e031aafadad7c66893
JulianMessner <julianmessner.13@gmail.com>
2024-03-02 16:40:30 +0100

    contact form style adjusted

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,16 +1,15 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgSwitch } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, NgSwitch],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
-
   form = {
     fullname: '',
     email: '',
@@ -18,13 +17,31 @@ export class ContactComponent {
     acceptPolicy: false,
   };
 
+  submitStatus: 'idle' | 'loading' | 'success' = 'idle';
+
   onSubmit(form: NgForm): void {
-    console.log(JSON.stringify(this.form));
-    form.resetForm();
+    this.submitStatus = 'loading';
+    
+    setTimeout(() => {
+      console.log(JSON.stringify(this.form));
+      form.resetForm();
+      this.submitStatus = 'success';
+      setTimeout(() => {
+        this.submitStatus = 'idle';
+      }, 2000);
+    }, 2000);
   }
 
-  onReset(form: NgForm): void {
-      form.resetForm();
-  } 
-
+  getButtonStatusText(): string {
+    switch (this.submitStatus) {
+      case 'idle':
+        return 'Send message :)';
+      case 'loading':
+        return 'Loading...';
+      case 'success':
+        return 'Message sent :)';
+      default:
+        return 'Send message :)';
+    }
+  }
 }
