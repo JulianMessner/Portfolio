79e9407a6d39249d9090c47ba87bba469536c490
JulianMessner <julianmessner.13@gmail.com>
2024-03-10 15:59:04 +0100

    portfolio responsive

--- src/app/main-content/portfolio/project-left/project-left.component.ts
+++ src/app/main-content/portfolio/project-left/project-left.component.ts
@@ -12,44 +12,32 @@ export class ProjectLeftComponent {
   imageSrcArray = [
     '../../../../assets/img/projects/join-project2.png',
     '../../../../assets/img/projects/el-pollo-loco.png',
-    '../../../../assets/img/projects/pc-mockup.png',
+    // '../../../../assets/img/projects/pc-mockup.png',
     '../../../../assets/img/projects/pokedex.png',
-    '../../../../assets/img/projects/pc-mockup.png',
-    '../../../../assets/img/projects/pc-mockup.png',
-    '../../../../assets/img/projects/pc-mockup.png',
-    '../../../../assets/img/projects/pc-mockup.png',
-    '../../../../assets/img/projects/pc-mockup.png',
-    '../../../../assets/img/projects/pc-mockup.png',
-    '../../../../assets/img/projects/pc-mockup.png',
-    '../../../../assets/img/projects/pc-mockup.png',
-    '../../../../assets/img/projects/pc-mockup.png',
-    '../../../../assets/img/projects/pc-mockup.png',
-    '../../../../assets/img/projects/pc-mockup.png',
-    '../../../../assets/img/projects/pc-mockup.png',
   ];
-  h3TitleArray = ['Join', 'El Pollo Loco', 'Simple CRM', 'Pokédex'];
+  h3TitleArray = ['Join', 'El Pollo Loco', 'Pokédex'];
   h4TitleArray = [
     'JavaScript | HTML | CSS',
     'JavaScript | HTML | CSS',
-    'Angular | TypeScript',
+    // 'Angular | TypeScript',
     'JavaScript | HTML | CSS | API',
   ];
   pTextArray = [
     'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
     'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to find bottles to fight against the mega chicken.',
-    'A simple Customer Relationship Management system working with CRUD functionality.',
+    // 'A simple Customer Relationship Management system working with CRUD functionality.',
     'Based on the PokéAPI a simple library that provides and catalogues pokemon information.',
   ];
   liveTestLinkArray = [
     'https://example.com/live-test-1',
     'https://example.com/live-test-2',
-    'https://example.com/live-test-3',
+    // 'https://example.com/live-test-3',
     'https://example.com/live-test-4',
   ];
   githubLinkArray = [
     'https://github.com/project1',
     'https://github.com/JulianMessner/El-Pollo-Loco',
-    'https://github.com/project3',
+    // 'https://github.com/project3',
     'https://github.com/JulianMessner/Pok-dex',
   ];
 }
